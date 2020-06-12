082be02b161bb09e9de42449254885b96982d465
rhoiotaphi <33744503+rhoiotaphi@users.noreply.github.com>
2020-06-12 16:22:37 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -19,22 +19,72 @@ data$pid <- as.numeric(data$pid)
 data$pid_scale <- ifelse(data$pid<4,data$pid, ifelse(data$pid==9,NA,data$pid-1))
 data$republican <- ifelse(data$pid>5 & data$pid<9,1,0)
 data$group <- as.numeric(data$group)
-data$expertise <- ifelse(data$group<5,1,ifelse(data$group==9,NA,0))
-data$fact <- ifelse(data$group<3 | data$group==5 | data$group==6,1,ifelse(data$group==9,NA,0))
-data$gain <- ifelse(data$group==2 | data$group==4| data$group==6 | data$group==8,1,ifelse(data$group==9,NA,0))
-
-
+data$expertise <- ifelse(data$group<5,1,ifelse(data$group==9,0,0))
+data$fact <- ifelse(data$group<3 | data$group==5 | data$group==6,1,ifelse(data$group==9,0,0))
+data$gain <- ifelse(data$group==2 | data$group==4| data$group==6 | data$group==8,1,ifelse(data$group==9,0,0))
+data$treatment <- ifelse(data$group==9,0,1)
+data$birth_year <- as.numeric(data$birth_year)
+data$ideology <- as.numeric(data$ideology)
 
 #Assigning Ethnicity
 data$ethnicity <- as.numeric(data$ethnicity)
 data$ethnicity <- ifelse(data$ethnicity>7,7,data$ethnicity)
 
+
 #Filtering low-attension out of the data
 data <- data[which(data$lowattention==0),]
+me
+means <- aggregate(value ~  group, data3, mean)
+
+#Summary Plot for Treatment Averages
+data2 <- data %>% select(group,treatment1, treatment2,treatment3,treatment4)
+data3 <- melt(data = data2, id.vars = "group", na.rm= T)
+data3$group <- as.factor(data3$group)
+means <- aggregate(value ~  group + variable, data3, mean)
+sds <- aggregate(value ~  group + variable, data3, sd)
+ggplot(data=data3,aes(x = group, y=value, fill=group)) + geom_boxplot(alpha=0.3) + theme(legend.position="none") +
+scale_fill_brewer(palette="BuPu")+ facet_wrap(~variable)  +scale_y_discrete(limits=c(1:7)) +
+  stat_summary(fun=mean, colour="black", geom="point",shape=18, size=0.1) +  
+geom_text(data = means, aes(label = round(value,2), y = value + 0.5), size=2, colour = "dark red")
+
+#Summary Plot for Ideology Scale Averages
+data2 <- data %>% select(ideology,treatment1, treatment2,treatment3,treatment4)
+data3 <- melt(data = data2, id.vars = "ideology", na.rm= T)
+data3$ideology <- as.factor(data3$ideology)
+means <- aggregate(value ~  ideology + variable, data3, mean)
+sds <- aggregate(value ~  ideology + variable, data3, sd)
+ggplot(data=data3,aes(x = ideology, y=value, fill=ideology)) + geom_boxplot(alpha=0.3) + theme(legend.position="none") +
+  scale_fill_brewer(palette="PuRd")+ facet_wrap(~variable)  +scale_y_discrete(limits=c(1:7)) +
+  stat_summary(fun=mean, colour="black", geom="point",shape=18, size=0.1) +  
+  geom_text(data = means, aes(label = round(value,2), y = value + 0.5), size=2, colour = "dark red")
+
+#Summary Plot for Party Scale Averages
+data2 <- data %>% select(pid_scale,treatment1, treatment2,treatment3,treatment4)
+data3 <- melt(data = data2, id.vars = "pid_scale", na.rm= T)
+data3$pid_scale <- as.factor(data3$pid_scale)
+means <- aggregate(value ~  pid_scale + variable, data3, mean)
+sds <- aggregate(value ~  pid_scale + variable, data3, sd)
+ggplot(data=data3,aes(x = pid_scale, y=value, fill=pid_scale)) + geom_boxplot(alpha=0.3) + theme(legend.position="none") +
+  scale_fill_brewer(palette="PuRd")+ facet_wrap(~variable)  +scale_y_discrete(limits=c(1:7)) +
+  stat_summary(fun=mean, colour="black", geom="point",shape=18, size=0.1) +  
+  geom_text(data = means, aes(label = round(value,2), y = value + 0.5), size=2, colour = "dark red")
+
+#Summary Plot for Media
+data2 <- data %>% select(fox,abc,fb,insta,twitter,nbc,cnn,nyt,treatment1, treatment2,treatment3,treatment4)
+data3 <- melt(data = data2, id.vars = c("treatment1", "treatment2","treatment3","treatment4"), na.rm= T)
+data3<-filter(data3,value==1)
+data3<-data3[,-6]
+data3 <- melt(data = data3, id.vars = "variable", na.rm= T)
+colnames(data3) <- c("media","treatment","value")
+means <- aggregate(value ~  media + treatment, data3, mean)
+ggplot(data=data3,aes(x = media, y=value, fill=media)) + geom_boxplot(alpha=0.3) + theme(legend.position="none") +
+  scale_fill_brewer(palette="RdYlBu")+ facet_wrap(~treatment)  +scale_y_discrete(limits=c(1:7)) +
+  stat_summary(fun=mean, colour="black", geom="point",shape=18, size=0.1) +  
+  geom_text(data = means, aes(label = round(value,2), y = value + 0.5), size=2, colour = "dark red")
 
 #Power Anlysis for T-Test
-power.t.test(n=4722,sig.level=0.05,power=0.9)
-#Delta of 0.067 which means we can detect a substatially small effect size
+power.t.test(n=502,sig.level=0.05,power=0.9)
+#Delta of 0.067 which means we can detect a small effect size
 
 #T-test comparing controls with others
 t.test(data$treatment1  ~  data$control)
@@ -123,7 +173,7 @@ model1e <- lm(treatment1 ~ expertise + gain + fact + pid_scale + expertise*pid_s
 summary(model1e)
 model1f <- lm(treatment1 ~ expertise + gain + fact + pid_scale + birth_year, data)
 summary(model1f)
-model1g <- lm(treatment1 ~ expertise + gain + fact + pid_scale + birth_year + fox, data)
+model1g <- lm(treatment1 ~ treatment + expertise + gain + fact + pid_scale + ideology + birth_year + fox + nbc, data)
 summary(model1g)
 
 #Regressions for Treatment2
@@ -139,7 +189,7 @@ model2e <- lm(treatment2 ~ expertise + gain + fact + pid_scale + expertise*pid_s
 summary(model2e)
 model2f <- lm(treatment2 ~ expertise + gain + fact + pid_scale + birth_year, data)
 summary(model2f)
-model2g <- lm(treatment2 ~ expertise + gain + fact + pid_scale + birth_year+ fox, data)
+model2g <- lm(treatment2 ~ treatment + expertise + gain + fact + pid_scale + ideology + birth_year + fox + nbc, data)
 summary(model2g)
 
 #Regressions for Treatment3
@@ -155,7 +205,7 @@ model3e <- lm(treatment3 ~ expertise + gain + fact + pid_scale + expertise*pid_s
 summary(model3e)
 model3f <- lm(treatment3 ~ expertise + gain + fact + pid_scale + birth_year, data)
 summary(model3f)
-model3g <- lm(treatment3 ~ expertise + gain + fact + pid_scale + birth_year +fox, data)
+model3g <- lm(treatment3 ~ treatment + expertise + gain + fact + pid_scale + ideology + birth_year + fox + nbc, data)
 summary(model3g)
 
 #Regressions for Treatment4
@@ -171,7 +221,7 @@ model4e <- lm(treatment4 ~ expertise + gain + fact + pid_scale + expertise*pid_s
 summary(model4e)
 model4f <- lm(treatment4 ~ expertise + gain + fact + pid_scale + birth_year, data)
 summary(model4f)
-model4g <- lm(treatment4 ~ expertise + gain + fact + pid_scale + birth_year + fox, data)
+model4g <- lm(treatment4 ~ treatment + expertise + gain + fact + pid_scale + ideology + birth_year + fox + nbc, data)
 summary(model4g)
 
 #Regression using Trust Variables
@@ -182,9 +232,26 @@ model <- lm(data$trust_doctors ~ data$republican)
 summary(model)
 
 #Plotting the models
-par(mfrow=c(2,2))
-plot_summs(model1g, omit.coefs = c("(Intercept)"),plot.distributions = F)
-plot_summs(model2g, omit.coefs = c("(Intercept)"),plot.distributions = F)
-plot_summs(model3g, omit.coefs = c("(Intercept)"),plot.distributions = F)
-plot_summs(model4g, omit.coefs = c("(Intercept)"),plot.distributions = F)
 
+plot_summs(model4g,model3g,model2g,model1g, omit.coefs = c("(Intercept)"),plot.distributions = F, scale=T,model.names = c("Treatment 1","Treatment 2","Treatment 3","Treatment 4"), legend.title = "Treatment") + theme(legend.position="bottom")
+
+
+#Distribution of Manipulation Checks
+table(data$mcscore,data$group)
+summary(aov(data$mcscore~data$group))
+
+data5<- data[which(is.na(data$pid_scale)),]
+model5a <- lm(treatment4 ~ control, data5)
+summary(model5a)
+model5b <- lm(treatment1 ~ expertise + gain+fact, data5)
+summary(model5b)
+model5c <- lm(treatment4 ~ expertise + gain + fact, data5)
+summary(model5c)
+model5d <- lm(treatment4 ~ expertise + gain + fact +ideology, data5)
+summary(model5d)
+model5e <- lm(treatment4 ~ expertise + gain + fact + pid_scale + expertise*pid_scale, data5)
+summary(model5e)
+model5f <- lm(treatment4 ~ expertise + gain + fact + pid_scale + birth_year, data5)
+summary(model5f)
+model5g <- lm(treatment4 ~ expertise + gain + fact + pid_scale + birth_year + fox, data5)
+summary(model5g)
